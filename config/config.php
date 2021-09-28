3a0ed01e375b20ae26f542401d6f3f091c4b24de
mirmac6 <91131062+mirmac6@users.noreply.github.com>
2021-09-28 13:49:11 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 $timezone = date_default_timezone_set("Europe/London");
 
-$con = mysqli_connect("sql11.freemysqlhosting.net", "sql11438943", "GSpufrRdCq", "sql11438943"); //Connection variable
+$con = mysqli_connect("eu-cdbr-west-01.cleardb.com", "b65cb4ee82bc31", "98def8df", "heroku_d4e8b5ad434fb73"); //Connection variable
 
 if(mysqli_connect_errno()) 
 {
